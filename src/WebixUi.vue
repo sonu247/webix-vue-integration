fc050b8983cf4cda7db4977cba0938a07bb73014
Sonu <sonu@chapter247.com>
2020-07-30 16:19:15 +0530

    Webix Pivot in Vue.JS

---
+++ src/WebixUi.vue
@@ -0,0 +1,49 @@
+<template>
+  <div></div>
+</template>
+<script>
+const webix = window.webix,
+  $$ = window.$$;
+
+function data_handler(value) {
+  var view = $$(this.webixId);
+
+  if (typeof value === "object") {
+    if (this.copyData) value = webix.copy(value);
+
+    if (view.setValues) view.setValues(value);
+    else if (view.parse) {
+      view.clearAll();
+      view.parse(value);
+    }
+  } else if (view.setValue) view.setValue(value);
+
+  webix.ui.each(
+    view,
+    function(sub) {
+      if (sub.hasEvent && sub.hasEvent("onValue"))
+        sub.callEvent("onValue", [value]);
+    },
+    this,
+    true
+  );
+}
+export default {
+  name: "WebixUi",
+  props: ["config", "value", "copyData"],
+  watch: {
+    value: {
+      handler: data_handler,
+    },
+  },
+  mounted: function() {
+    var config = webix.copy(this.config);
+    config.$scope = this;
+    this.webixId = webix.ui(config, this.$el);
+    if (this.value) data_handler.call(this, this.value);
+  },
+  destroyed: function() {
+    webix.$$(this.webixId).destructor();
+  },
+};
+</script>
